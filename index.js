b40bbce16e7da4c439d37fcde9ae03f4d2fe2c03
Juan Diego <juandiegar@gmail.com>
2023-10-23 21:58:47 -0500

    primero pasosen node

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const a = 'hola como estas este es un test de como se ve'
-console.log(a)
+const os = require('node:os')
+console.log("Hola como estas este es una app que te da info")
+console.log(os.platform())
+console.log(os.cpus())
+console.log(os.totalmem()/1024/1024/1024)
+console.log("tiempo en linea",os.uptime()/60/60/24)
+console.log("memoria libre",os.freemem()/1024/1024/1024)
+
